7763227b3f66191b3352a68d4377a27b9b5f76cd
Arthope <pochtawiw345@gmail.com>
2024-01-16 21:45:01 +0300

    Init

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private int _numberBalls;
+    public float speed = 3f;
+    [SerializeField] private List<Transform> _currentWaypoints;
+    private int _currentWaypointIndex = 0;
+    private Rigidbody _rigidbody;
+    private FinishTrigger _finishTrigger;
+    private float _minimalDistance = 0.6f;
+    [SerializeField] bool _finished = false;
+    [SerializeField] private BallMovement _targetBall;
+    [SerializeField] private bool _obstacleAhead = false;
+  //  [SerializeField] private bool _finished= false;
+
+   
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _finishTrigger = FindObjectOfType<FinishTrigger>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+         MoveToNextPoint();
+             
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.TryGetComponent<BallMovement>(out BallMovement ball))
+        {
+            if (ball._numberBalls == _numberBalls - 1)
+            {
+               _rigidbody.constraints = RigidbodyConstraints.FreezePosition;
+                _targetBall = ball;
+                _obstacleAhead = true;
+            }
+        }
+        if (collider.gameObject.TryGetComponent<FinishTrigger>(out FinishTrigger finish))
+        {
+            if (_finished == false)
+            {
+                finish.CountBalls();
+                _finished = true;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.TryGetComponent<BallMovement>(out BallMovement ball))
+        {
+             _rigidbody.constraints = RigidbodyConstraints.None;
+              MoveToNextPoint();
+            _obstacleAhead = false;
+        }
+    }
+
+
+    public void MoveToNextPoint()
+    {
+        if (_currentWaypoints != null && _currentWaypointIndex < _currentWaypoints.Count)
+        { 
+            Vector3 moveDerection = (_currentWaypoints[_currentWaypointIndex].position - transform.position).normalized;       
+           _rigidbody.velocity = moveDerection * speed;
+             if (Vector3.Distance(transform.position, _currentWaypoints[_currentWaypointIndex].position) <= _minimalDistance)
+             {
+                _currentWaypointIndex++;
+             }
+             else
+             {
+                _rigidbody.AddForce(0, 0, -1 * speed);
+
+             }
+        }
+    }
+
+    public void Destroy()
+    {
+        _finishTrigger.TakeAwayBall();
+        Destroy(gameObject);
+    }
+
+    public void MoveToWaypoints(List<Transform> waypoints) 
+    {
+        _currentWaypoints = waypoints;
+        _currentWaypointIndex = 0;
+        MoveToNextPoint();
+    }
+
+    public void KeepMoving()
+    {
+        _rigidbody.constraints = RigidbodyConstraints.None;      
+    }
+     
+    public int GetNumberBalls(int numberball)
+    {
+        _numberBalls = numberball;
+        return numberball;      
+    }
+}
---
+++ Assets/Scripts/FinishTrigger.cs
@@ -0,0 +1,125 @@
+using Agava.YandexGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour
+{
+    private const string LeaderboardName = "LeaderBoard";
+    [SerializeField] GameObject _canvasFinish;
+    [SerializeField] GameObject _firstStar;
+    [SerializeField] GameObject _secondStar;
+    [SerializeField] GameObject _thirdStar;
+    [SerializeField] private SpawnBalls _spawn;
+    [SerializeField] private GameObject _canvasGameOver;
+    [SerializeField] private string _levelName;
+    private int _totalNumberStars = 0;
+    private float _currentAmountBalls = 0;
+    private float _spawnCount;
+    private float _currentPercent;
+    private float _currentSpawnCount;
+
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("_currentStars"))
+        {
+            _totalNumberStars = PlayerPrefs.GetInt("_currentStars");
+        }
+                             
+        _spawnCount = _spawn.SpawnCount;
+        
+        _currentSpawnCount = _spawn.SpawnCount;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.TryGetComponent<BallMovement>(out BallMovement ballComponent))
+        {
+            Debug.Log("bam");
+        }    
+    }
+
+    private void Finish()
+    {
+        _currentPercent = _currentAmountBalls / _spawnCount * 100f;
+        Debug.Log(_currentPercent.ToString());
+        int totalStars = 0;
+        
+        if (_currentPercent < 30)
+        {
+            _canvasGameOver.SetActive(true);
+        }
+        if (_currentPercent >= 30)
+        {
+            _canvasFinish.SetActive(true);
+            _firstStar.SetActive(true);
+            ChargingStats();
+            totalStars++;
+        }
+        if (_currentPercent >= 60)
+        {
+            _secondStar.SetActive(true);
+            ChargingStats();
+            totalStars++;
+
+        }
+        if (_currentPercent >= 90)
+        {
+            _thirdStar.SetActive(true);
+            ChargingStats();
+            totalStars++;
+
+        }
+        Time.timeScale = 0f;
+        PlayerPrefs.SetInt(_levelName, totalStars);
+
+    }
+
+    public void CountBalls()
+    {
+        _currentAmountBalls++;
+        Debug.Log(_currentAmountBalls.ToString() + " число которое посчитал финиш");
+        Debug.Log(_currentSpawnCount + " число которое пришло от спавнера");
+        if (_currentSpawnCount == _currentAmountBalls)
+        {
+            Finish();
+        }
+    }
+
+   
+
+    public void TakeAwayBall()
+    {
+        _currentSpawnCount--;
+        if (_currentSpawnCount <= 0)
+        {
+            _canvasGameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    
+    private void ChargingStats()
+    {
+        if (PlayerPrefs.HasKey("_currentStars"))
+        {
+            _totalNumberStars = PlayerPrefs.GetInt("_currentStars");
+            PlayerPrefs.SetInt("_currentStars", _totalNumberStars++);
+            PlayerPrefs.Save();
+            if (PlayerAccount.IsAuthorized)
+            {
+                Agava.YandexGames.Leaderboard.SetScore(LeaderboardName, _totalNumberStars);
+            }
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt("_CurrentStars", _totalNumberStars++);
+            PlayerPrefs.Save();
+            if (PlayerAccount.IsAuthorized)
+            {
+                Agava.YandexGames.Leaderboard.SetScore(LeaderboardName, _totalNumberStars);
+            }
+        }
+    }
+}
